490b0d0f43fb4871b0037a86b090fce3780d1dd4
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-07-07 10:46:09 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -1,8 +1,4 @@
 document.querySelector(".navbar").style.setProperty('background-color', '#0D47A1', 'important');
- const logoutBtn = document.querySelector("body > div.wrapper > div.main-panel.ps > nav > div > div.collapse.navbar-collapse.justify-content-end > ul > li.nav-item.dropdown.pfile.dd > div > a:nth-child(5)");
-  if (logoutBtn) {
-    logoutBtn.onclick = function () {
-        alert("bingo");
-        Android.logOut();
-    };
-  }
+if(window.screen.width<365){
+document.querySelector("body > div > div > div.container > div").style.marginTop="5em";
+}
