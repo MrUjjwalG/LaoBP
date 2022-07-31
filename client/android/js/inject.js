79edc5c2f901fcb8e59b57d8f6229e29931ec9b1
Ujjwal Kashyap <mr.ujjwalg@gmail.com>
2022-08-01 02:00:10 +0530

    Update inject.js

--- client/android/js/inject.js
+++ client/android/js/inject.js
@@ -5,7 +5,6 @@ if(window.location.href=="https://www.laobp.in/Home/Registration"){
         document.querySelector("body > div > div > div.container > div").style.marginTop="10em";
     }
 }
-   document.querySelector(".navbar").style.setProperty('background-color', '#0D47A1', 'important');
 
 if(document.querySelector("body > div.wrapper > div.main-panel.ps > nav > div > div.navbar-wrapper > a:nth-child(2) > label")){
  Android.setVendorName(document.querySelector("body > div.wrapper > div.main-panel.ps > nav > div > div.navbar-wrapper > a:nth-child(2) > label").innerHTML);
